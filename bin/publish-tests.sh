1b69ecd91a196087e4807c80f6f8bcefaa638a16
Ivar Abrahamsen <ivar.abrahamsen@defra.gov.uk>
2024-02-06 17:04:27 +0000

    publishing test results

--- bin/publish-tests.sh
+++ bin/publish-tests.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-DIRECTORY=./allure-report
+DIRECTORY="$PWD/allure-report"
+
+echo "Publishing test results to S3"
 
 if [ -n "$RESULTS_OUTPUT_S3_PATH" ]; then
    if [ -d "$DIRECTORY" ]; then
       aws s3 cp "$DIRECTORY" "$RESULTS_OUTPUT_S3_PATH" --recursive
+      echo "Test results published to $RESULTS_OUTPUT_S3_PATH"
    else
       echo "$DIRECTORY is not found"
       exit 1
