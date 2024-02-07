216ff4ef35f9fd43c6dbf226773325a05070797c
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-02-07 08:45:40 +0000

    Prevents chrome/driver being downloaded when using remote selenium instance

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,6 +17,10 @@ export const config = {
   // gets prepended directly.
   baseUrl: process.env.BASE_URL || 'http://localhost:3000',
 
+  // Connection to remote chromedriver
+  hostname: process.env.CHROMEDRIVER_URL || '127.0.0.1',
+  port: process.env.CHROMEDRIVER_PORT || 4444,
+
   //
   // ==================
   // Specify Test Files
@@ -64,10 +68,7 @@ export const config = {
 
   capabilities: [
     {
-      browserName: 'chrome',
-      hostname: process.env.CHROMEDRIVER_URL || '127.0.0.1',
-      port: process.env.CHROMEDRIVER_PORT | 4444,
-      'wdio:chromedriverOptions': {}
+      browserName: 'chrome'
     }
   ],
 
