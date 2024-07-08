b41fa5bf70aa2e79e62d039d40cbefbc2e50c51d
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-07-08 19:52:49 +0100

    rolls back browserstack integration

--- test-proxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "testing proxy https://$SQUID_USERNAME:*****@proxy.$ENVIRONMENT.cdp-int.defra.cloud"
-echo "testing browserstack"
-curl -v -x "https://$SQUID_USERNAME:$SQUID_PASSWORD@proxy.$ENVIRONMENT.cdp-int.defra.cloud" "https://hub.browserstack.com"
-echo "testing github"
-curl -v -x "https://$SQUID_USERNAME:$SQUID_PASSWORD@proxy.$ENVIRONMENT.cdp-int.defra.cloud" "https://api.github.com"
