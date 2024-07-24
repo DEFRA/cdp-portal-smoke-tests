1531497604f285ecc25acb8b7af6b8a633057a96
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-07-24 19:28:23 +0100

    attempt to reproduce cookie error

--- test/specs/services.e2e.js
+++ test/specs/services.e2e.js
@@ -4,6 +4,10 @@ import ServicePage from 'page-objects/services.page'
 
 describe('Service page', () => {
   it('Should be on the "Service" page', async () => {
+    await browser.deleteCookies(['userSession'])
+    await browser.deleteCookies(['cdpPortalSession'])
+    await browser.url('')
+    await browser.maximizeWindow()
     await ServicePage.open()
 
     await expect(browser).toHaveTitle(
--- test/specs/teams.e2e.js
+++ test/specs/teams.e2e.js
@@ -4,6 +4,11 @@ import TeamsPage from 'page-objects/teams.page'
 
 describe('Teams page', () => {
   it('Should be on the "Teams" page', async () => {
+    await browser.deleteCookies(['userSession'])
+    await browser.deleteCookies(['cdpPortalSession'])
+    await browser.url('')
+    await browser.maximizeWindow()
+
     await TeamsPage.open()
     await expect(browser).toHaveTitle('Teams | Core Delivery Platform - Portal')
     await expect(TeamsPage.pageHeading).toHaveText('Teams')
